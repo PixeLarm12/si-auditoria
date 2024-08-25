10feaa737aefc5674e4ecaa866b3385f32295c47
Lucas Domingues <lucasrdomingues-lrd@hotmail.com>
2024-08-25 19:09:45 -0300

    Added month balance

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 data = pd.read_csv('auditoria.csv')
 
+# Check day that more than one transaction occurred
 credit = data[data['Operação'] == 'Crédito']
 debit = data[data['Operação'] == 'Débito']
 
@@ -16,3 +17,18 @@ print(moreThanOneCredit)
 
 print("\nData onde são mais de 1 lançamento de Débito:")
 print(moreThanOneDebit)
+
+# Check if month balance are ZERO
+data['Data'] = pd.to_datetime(data['Data'], format='%d/%m/%Y')
+
+data['Valor'] = pd.to_numeric(data['Valor'], errors='coerce')
+
+data['Mes'] = data['Data'].dt.to_period('M')
+
+data['Valor'] = data.apply(lambda x: x['Valor'] if x['Operação'] == 'Débito' else -x['Valor'], axis=1)
+monthBalance = data.groupby('Mes')['Valor'].sum()
+
+monthBalanceError = monthBalance[monthBalance != 0]
+
+print("Meses com saldo não zerado:")
+print(monthBalanceError)
