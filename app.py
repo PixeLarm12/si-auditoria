4857940a16c558a431a5b934852db5bdf5de92eb
Lucas Domingues <lucasrdomingues-lrd@hotmail.com>
2024-08-25 18:58:42 -0300

    Fixxed

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
-print("hello world")
+import pandas as pd
+
+data = pd.read_csv('auditoria.csv')
+
+credit = data[data['Operação'] == 'Crédito']
+debit = data[data['Operação'] == 'Débito']
+
+creditPerDay = credit.groupby('Data').size()
+debitPerDay = debit.groupby('Data').size()
+
+moreThanOneCredit = creditPerDay[creditPerDay > 1]
+moreThanOneDebit = debitPerDay[debitPerDay > 1]
+
+print("Data onde são mais de 1 lançamento de Crédito:")
+print(moreThanOneCredit)
+
+print("\nData onde são mais de 1 lançamento de Débito:")
+print(moreThanOneDebit)
